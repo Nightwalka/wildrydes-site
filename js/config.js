745c0f6f8d716b4ac975e8073f8cc238d1e28051
Nightwalka <60555740+Nightwalka@users.noreply.github.com>
2025-01-19 22:59:44 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_6PB1z8sM6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27mqfnh1u5bs75abal96u1kv7j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_KjXcO6yzo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34vk9kc6ikbberonkdvtgi0iih', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
