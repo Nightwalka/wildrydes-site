dba2d5e5f4d8b4e66f3b2a367caafd173333825f
Nightwalka <60555740+Nightwalka@users.noreply.github.com>
2025-01-19 22:48:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_6PB1z8sM6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '27mqfnh1u5bs75abal96u1kv7j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
